a292118f957c61abf417e7c52627c6a38e789e5f
gargr01099 <rahulgarg.eminence@gmail.com>
2024-10-28 12:25:30 +0530

    balance

--- readBlockchain.js
+++ readBlockchain.js
@@ -5,5 +5,8 @@ const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/69009c
 const queryBlockchain = async()=>{
     const block = await provider.getBlockNumber();
     console.log("Cuurent block number",block);
-}
+
+// const balance = await provider.getBalance("0x00000000219ab540356cbb839cbe05303d7705fa");
+// console.log("Balance of the address",balance);
 queryBlockchain();
+}
